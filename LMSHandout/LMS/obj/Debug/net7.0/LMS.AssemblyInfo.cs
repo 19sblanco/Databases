8f427b59635cd283d94b38db541c854acf4e3b97
19sblanco <steven.blanc521@gmail.com>
2024-04-02 18:48:58 -0600

    added scafolding and connected to both LMS DB and LMS user DB

--- LMSHandout/LMS/obj/Debug/net7.0/LMS.AssemblyInfo.cs
+++ LMSHandout/LMS/obj/Debug/net7.0/LMS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02fcfd63695ec6f79cae35bb57db95083b0e3b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
